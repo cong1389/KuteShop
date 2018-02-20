b6e1f670d74124a81ddebb3af0238f443ff04632
SUCCESS-PC\SUCCESS <truong.thanhcong89>
2018-02-20 09:17:13 +0700

    GUI checkout

---
+++ App.Front/Models/Posts/CategoryPostModel.cs
@@ -0,0 +1,26 @@
+﻿using App.Domain.Entities.Menu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using App.Domain.Entities.Data;
+
+namespace App.Front.Models.Posts
+{
+    public class CategoryPostModel
+    {
+        public int NumberMenu { get; set; }
+
+        public CategoryPostModel()
+        {
+            MenuLinks= new List<MenuLink>();
+            Posts = new List<Post>();
+        }
+
+        public IEnumerable<MenuLink> MenuLinks;
+
+        public IEnumerable<Post> Posts;
+
+
+    }
+}
---
+++ App.Front/Themes/Basic/Scripts/goweb.common.js
@@ -0,0 +1,10 @@
+﻿
+if (typeof EGA === 'undefined') EGA = {};
+EGA.product = {};
+EGA.plugin = {};
+EGA.addScript = {};
+EGA.header = {};
+EGA.filter = {};
+EGA.options = {
+    money_format: "{{amount}}₫",
+}
