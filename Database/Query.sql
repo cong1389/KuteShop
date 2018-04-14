3ae68d0a1051619c1989b7e8f1b85bb51a8abe3a
SUCCESS-PC\SUCCESS <truong.thanhcong89>
2018-04-14 08:57:01 +0700

    error, don't save shoppingcartitem

--- Database/Query.sql
+++ Database/Query.sql
@@ -1,5 +1,14 @@
+use SmartStoreV303
+go
+
 use KuteShop
 GO
 
-select * from MenuLink
-select * from Post
+select Id,ParentId,MenuName, * from MenuLink where Position=5
+
+select * from [Order]
+
+
+truncate table [Order]
+
+delete from [Order]
