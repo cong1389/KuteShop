f5fd667cb541f13e2c41e9b3b35b0428a53ad6c3
cong.truong <con.truong@vn.rightmarket.com>
2018-06-11 16:22:54 +0700

    Plugin

--- App.Admin/Areas/Admin/Controllers/PluginController.cs
+++ App.Admin/Areas/Admin/Controllers/PluginController.cs
@@ -1,4 +1,6 @@
-﻿using App.Admin.Areas.Admin.Extensions;
+﻿using System;
+using System.Collections.Generic;
+using App.Admin.Areas.Admin.Extensions;
 using App.Admin.Areas.Model.Plugins;
 using App.Core.Extensions;
 using App.Core.Plugins;
@@ -12,8 +14,8 @@ namespace App.Admin.Controllers
 {
 	public class PluginController : BaseAdminController
 	{
-        private readonly IPluginFinder _pluginFinder;
-	    private readonly ICommonServices _services;
+		private readonly IPluginFinder _pluginFinder;
+		private readonly ICommonServices _services;
 
 		public PluginController(IPluginFinder pluginFinder, ICommonServices services)
 		{
@@ -21,82 +23,109 @@ namespace App.Admin.Controllers
 			_services = services;
 		}
 
-        // GET: Admin/Plugin
-        public ActionResult Index()
-        {
-            var model = PrepareLocalPluginsModel();
+		// GET: Admin/Plugin
+		public ActionResult Index()
+		{
+			var model = PrepareLocalPluginsModel();
 
-            return View(model);
-        }
+			return View(model);
+		}
 
-        [NonAction]
-        protected LocalPluginsModel PrepareLocalPluginsModel()
-        {
-            var plugins = _pluginFinder.GetPluginDescriptors(false)
-                .OrderBy(p => p.Group, PluginFileParser.KnownGroupComparer)
-                .ThenBy(p => p.DisplayOrder)
-                .Select(x => PreparePluginModel(x));
+		[NonAction]
+		protected LocalPluginsModel PrepareLocalPluginsModel()
+		{
+			var plugins = _pluginFinder.GetPluginDescriptors(false)
+				.OrderBy(p => p.Group, PluginFileParser.KnownGroupComparer)
+				.ThenBy(p => p.DisplayOrder)
+				.Select(x => PreparePluginModel(x));
 
-            var model = new LocalPluginsModel();
-            
-            var groupedPlugins = from p in plugins
-                group p by p.Group into g
-                select g;
+			var model = new LocalPluginsModel();
 
-            foreach (var group in groupedPlugins)
-            {
-                foreach (var plugin in group)
-                {
-                    model.Groups.Add(group.Key, plugin);
-                }
-            }
+			var groupedPlugins = from p in plugins
+								 group p by p.Group into g
+								 select g;
 
-            return model;
-        }
+			foreach (var group in groupedPlugins)
+			{
+				foreach (var plugin in group)
+				{
+					model.Groups.Add(group.Key, plugin);
+				}
+			}
+
+			return model;
+		}
 
 
-        [NonAction]
-        protected PluginModel PreparePluginModel(PluginDescriptor pluginDescriptor, bool forList = true)
-        {
-            var model = pluginDescriptor.ToModel();
+		[NonAction]
+		protected PluginModel PreparePluginModel(PluginDescriptor pluginDescriptor, bool forList = true)
+		{
+			var model = pluginDescriptor.ToModel();
 
-            // Using GetResource because T could fallback to NullLocalizer here.
-            model.Group = _services.Localization.GetResource("Admin.Plugins.KnownGroup." + pluginDescriptor.Group);
+			// Using GetResource because T could fallback to NullLocalizer here.
+			model.Group = _services.Localization.GetResource("Admin.Plugins.KnownGroup." + pluginDescriptor.Group);
 
-            if (forList)
-            {
-                model.FriendlyName = "FriendlyName";
-                model.Description = "Description";
-            }
+			if (forList)
+			{
+				model.FriendlyName = pluginDescriptor.FriendlyName;
+				model.Description = pluginDescriptor.Description;
+			}
 
-            // Icon
-            //model.IconUrl = _pluginMediator.GetIconUrl(pluginDescriptor);
+			// Icon
+			//model.IconUrl = _pluginMediator.GetIconUrl(pluginDescriptor);
 
-            if (pluginDescriptor.Installed)
-            {
-                // specify configuration URL only when a plugin is already installed
-                if (pluginDescriptor.IsConfigurable)
-                {
-                    //model.ConfigurationUrl = Url.Action("ConfigurePlugin", new { systemName = pluginDescriptor.SystemName });
+			if (pluginDescriptor.Installed)
+			{
+				// specify configuration URL only when a plugin is already installed
+				if (pluginDescriptor.IsConfigurable)
+				{
+					//model.ConfigurationUrl = Url.Action("ConfigurePlugin", new { systemName = pluginDescriptor.SystemName });
 
-                    if (!forList)
-                    {
-                        var configurable = pluginDescriptor.Instance() as IConfigurable;
+					if (!forList)
+					{
+						var configurable = pluginDescriptor.Instance() as IConfigurable;
 
-                        string actionName;
-                        string controllerName;
-                        RouteValueDictionary routeValues;
-                        configurable.GetConfigurationRoute(out actionName, out controllerName, out routeValues);
+						string actionName;
+						string controllerName;
+						RouteValueDictionary routeValues;
+						configurable.GetConfigurationRoute(out actionName, out controllerName, out routeValues);
 
-                        if (actionName.HasValue() && controllerName.HasValue())
-                        {
-                           // model.ConfigurationRoute = new RouteInfo(actionName, controllerName, routeValues);
-                        }
-                    }
-                }
-            }
+						if (actionName.HasValue() && controllerName.HasValue())
+						{
+							//model.ConfigurationRoute = new RouteInfo(actionName, controllerName, routeValues);
+						}
+					}
+				}
+			}
 
-            return model;
-        }
-    }
+			return model;
+		}
+
+		[HttpPost]
+		public ActionResult ExecuteTask(string pluginToInstall, string pluginsToUninstall)
+		{
+			try
+			{
+				IEnumerable<PluginDescriptor> descriptors = null;
+				if (!string.IsNullOrEmpty(pluginToInstall))
+				{
+					descriptors = _pluginFinder.GetPluginDescriptors(false).Where(x => pluginToInstall.Contains(x.SystemName));
+					foreach (var d in descriptors)
+					{
+						if (!d.Installed)
+						{
+							d.Instance().Install();
+						}
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e);
+				throw;
+			}
+
+			return RedirectToAction("Index");
+		}
+	}
 }
